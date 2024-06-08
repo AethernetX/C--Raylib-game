86ca430651130d83d10c35a517bfd56562207367
Tam <mthanimr24@gmail.com>
2024-06-08 23:17:47 +0100

    Raylibs-cs experiment

---
+++ Entity.cs
@@ -0,0 +1,51 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace game
+{
+
+    public class Entity
+    {
+        int width;
+        int height;
+        int posX, posY;
+
+        Vector2 size;
+        Vector2 pos;
+        Vector2 Accel;
+        int Mass = 10;
+
+        public Entity(int width, int height, int posX, int posY)
+        {
+            this.width = width;
+            this.height = height;
+            this.posX = posX;
+            this.posY = posY;
+
+            size = new Vector2(width, height);
+            pos = new Vector2(posX, posY);
+        }
+
+        public void draw()
+        {
+            //Raylib.DrawRectangle(posX, posY, width, height, Color.Blue);
+            Raylib.DrawRectangleV(pos, size, Color.Red);
+        }
+
+        public void Update()
+        {
+            // F = MA
+            // A = F/M
+
+            Raylib.DrawText(Accel.Y.ToString(), 100, 100, 20, Color.White);
+
+            if (pos.Y < Raylib.GetScreenHeight() - size.Y) 
+            {
+                Accel.Y += 19.6f * Raylib.GetFrameTime();
+                pos.Y += Accel.Y * Raylib.GetFrameTime();
+            }
+
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Raylib_cs;
+using System.Numerics;
+using game;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.SetTargetFPS(60);
+
+        Vector2 example = new Vector2(100f, 100f);
+
+        Raylib.InitWindow(1280, 720, "Game");
+
+        Entity x = new Entity(100, 100, 100, 100);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Black);
+
+            x.draw();
+            x.Update();
+
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
